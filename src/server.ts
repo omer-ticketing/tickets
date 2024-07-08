ec42b3fef7c1086e41dd19575f5bada160cdc673
omer <omerme13@gmail.com>
2024-07-08 19:23:25 +0300

    fix: using MONGO_URI from env variables

--- src/server.ts
+++ src/server.ts
@@ -6,18 +6,21 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
-const connectToDB = async () => {
-	await mongoose.connect('mongodb://tickets-mongo-srv:27017/tickets');
-	console.log("Connected to mongoDB...");
-};
-
 // TODO check if this is the right place for the err
 if (!process.env.JWT_SECRET) {
 	throw new Error("JWT secret must be defined.")
 }
 
-connectToDB();
+if (!process.env.MONGO_URI) {
+	throw new Error('MONGO_URI must be defined.')
+}
 
+const connectToDB = async () => {
+	await mongoose.connect(process.env.MONGO_URI!);
+	console.log("Connected to mongoDB...");
+};
+
+connectToDB();
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}...`);
