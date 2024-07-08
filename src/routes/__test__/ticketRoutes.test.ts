fee9e5d3412341a08a66e8c0bbeebe5ce4869339
omer <omerme13@gmail.com>
2024-07-08 21:57:19 +0300

    more tests

--- src/routes/__test__/ticketRoutes.test.ts
+++ src/routes/__test__/ticketRoutes.test.ts
@@ -1,26 +1,24 @@
-import request from 'supertest';
-import app from '../../app';
+import request from "supertest";
+import app from "../../app";
 
-it('Has a route handler listening to /api/tickets for post requests', async () => {
-	const response = await request(app)
-		.post('/api/tickets')
-		.send({})
+it("Has a route handler listening to /api/tickets for post requests", async () => {
+    const response = await request(app).post("/api/tickets").send({});
 
-	expect(response.status).not.toEqual(404)
-})
+    expect(response.status).not.toEqual(404);
+});
 
-it('Can only be accessed if the user is signed in.', async () => {
+it("Can only be accessed if the user is signed in.", async () => {
+    await request(app).post("/api/tickets").send({}).expect(401);
+});
 
-})
+it("Returns a status other that 401 if the user is signed in.", async () => {
+    const response = await request(app).post("/api/tickets").send({});
 
-it('Returns an error if an invalid title is provided.', async () => {
+    expect(response.status).not.toEqual(401);
+});
 
-})
+it("Returns an error if an invalid title is provided.", async () => {});
 
-it('Returns an error if an invalid price is provided.', async () => {
+it("Returns an error if an invalid price is provided.", async () => {});
 
-})
-
-it('Creates a ticket with valid inputs', async () => {
-
-})
+it("Creates a ticket with valid inputs", async () => {});
--- src/routes/ticketsRoutes.ts
+++ src/routes/ticketsRoutes.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import * as ticketsController from '../controllers/ticketsControllers';
+import { authMiddlewares } from '@omer-ticketing/common';
 
 const router = express.Router();
 
-router.post('/', ticketsController.createNewTicket);
+router.post('/',authMiddlewares.protect, ticketsController.createNewTicket);
 
 export default router;
