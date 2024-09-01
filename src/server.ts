74719aec17e72438f86dc5761d65703ec84fdb08
omer <omerme13@gmail.com>
2024-09-01 19:37:05 +0300

    fix(server): change the order of calling the listeners

--- src/server.ts
+++ src/server.ts
@@ -21,14 +21,14 @@ const init = async () => {
     ifNotExistThrowErr(process.env.NATS_URL, "NATS URL");
     ifNotExistThrowErr(process.env.NATS_CLUSTER_ID, "NATS cluster Id");
 
-	initNATSListeners();
     await natsWrapper.connect(process.env.NATS_CLUSTER_ID!, process.env.NATS_CLIENT_ID!, process.env.NATS_URL!);
     natsWrapper.client.on("close", () => {
-        console.log("Nats connection closed.");
+		console.log("Nats connection closed.");
         process.exit();
     });
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+	initNATSListeners();
 
     await mongoose.connect(process.env.MONGO_URI!);
     console.log("Connected to mongoDB...");
